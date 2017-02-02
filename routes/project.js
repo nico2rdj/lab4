351375f2801b79407ecb0716ae62ba776518878b
Nicolas Martin <njm003@ucsd.edu>
2017-02-01 16:50:02 -0800

    node.js lab

---
+++ routes/project.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+exports.viewProject = function(req, res) {
+  // controller code goes here 
+  var name =  req.params.name;
+  console.log("The project name is: " + name);
+  res.render('project', {
+    'projectName': name
+  });
+
+};
